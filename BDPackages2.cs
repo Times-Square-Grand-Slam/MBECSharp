c0e71df7039d7e8ef1a7486cbb444e6102a6d63e
Rodney Haskins <it_rodney_haskins@hotmail.com>
2019-10-20 20:57:14 -0500

    Update RH

--- BDPackages2.cs
+++ BDPackages2.cs
@@ -16,5 +16,40 @@ namespace MBECSharp
         {
             InitializeComponent();
         }
+
+        private void BnFood_Click(object sender, EventArgs e)
+        {
+            new Food().Show();
+        }
+
+        private void BnMovie_Click(object sender, EventArgs e)
+        {
+            new Movie().Show();
+        }
+
+        private void BnBowl_Click(object sender, EventArgs e)
+        {
+            new Bowling().Show();
+        }
+
+        private void BnArcade_Click(object sender, EventArgs e)
+        {
+            new Arcade().Show();
+        }
+
+        private void BnAct_Click(object sender, EventArgs e)
+        {
+            new Activities().Show();
+        }
+
+        private void BnPartyArea_Click(object sender, EventArgs e)
+        {
+            new Area().Show();
+        }
+
+        private void BnMisc_Click(object sender, EventArgs e)
+        {
+            new Misc().Show();
+        }
     }
 }
