5f90269c04c3c6a95099cb74610c478b4eb57d9b
PDarden93 <49132793+PDarden93@users.noreply.github.com>
2019-11-05 20:38:47 -0600

    Publishing!!
    Getting everything ready for publishing. Right now it will publish to IT\MarketingEventsApplication\.
    
    Updates will need to go to IT\MarketingEventsApplication\Updates\.
    
    Need to get the files needed to run in the correct place before it will be able to install.
    
    When this is figured out, will do the same to the dev channel.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MBECSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Marketing Booking Events")]
+[assembly: AssemblyDescription("Application used to book events and parties.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Times Square Grand Slam")]
 [assembly: AssemblyProduct("MBECSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
